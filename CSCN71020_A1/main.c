2f6ebe4e5325d2a50b145aad09702c4e61883e67
SrujanReddy <svadde7996@conestogac.on.ca>
2022-11-03 19:00:55 -0400

    add function is corrected

--- CSCN71020_A1/main.c
+++ CSCN71020_A1/main.c
@@ -39,15 +39,18 @@ void printOptions() {
 	printf("2. Subtract\n");
 }
 
-void add() {
+void add()
+{
 	double num1, num2, result;
 	printf("Enter the first value:");
 	scanf_s("%lf", &num1);
 	printf("Enter the second value:");
 	scanf_s("%lf", &num2);
 	result = num1 + num2;
-	printf("%lf + %lf = %lf\n", num1, num2, result);
+	printf("Result is %lf\n", result);
+	exit(1);
 }
+
 void substract()
 {
 	double num1, num2, result;
